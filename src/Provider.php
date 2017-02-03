e4299051fd957282b21539fa53b2e0f3915c1350
James Cullen <jamcullenio@gmail.com>
2017-02-03 18:19:52 +0000

    Removed unnecessary scopes

--- src/Provider.php
+++ src/Provider.php
@@ -16,18 +16,7 @@ class Provider extends AbstractProvider implements ProviderInterface
     /**
      * {@inheritdoc}
      */
-    protected $scopes = [
-        'read_content', 'write_content',
-        'read_themes', 'write_themes',
-        'read_products', 'write_products',
-        'read_customers', 'write_customers',
-        'read_orders', 'write_orders',
-        'read_script_tags', 'write_script_tags',
-        'read_fulfillments', 'write_fulfillments',
-        'read_shipping', 'write_shipping',
-        'read_analytics',
-        'read_users', 'write_users'
-    ];
+    protected $scopes = [];
 
     /**
      * {@inheritdoc}
